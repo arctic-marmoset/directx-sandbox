d9587052e0805e6d57c9f379ca3e56ccd2b66864
ArcticMarmoset <45583929+ArcticMarmoset@users.noreply.github.com>
2020-06-14 19:44:38 -0400

    Use WinMain again to fix Release mode

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,14 +1,21 @@
 #include "gspch.h"
 #include "Sandbox/Window.h"
+#include "Sandbox/Clock.h"
 
 int main(int argc, char *argv[])
 {
+    return WinMain(GetModuleHandle(nullptr), nullptr, GetCommandLine(), SW_SHOWDEFAULT);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
     Window window;
-    window.ShowWindow(SW_SHOWDEFAULT);
+    window.ShowWindow(nCmdShow);
 
     while (Window::ProcessMessages())
     {
         window.OnUpdate();
+
     }
 
     return 0;
