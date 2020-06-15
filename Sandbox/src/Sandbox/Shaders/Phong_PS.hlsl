21cdd97a5e9d578a5c1ea01fde8e743973db49b9
ArcticMarmoset <45583929+ArcticMarmoset@users.noreply.github.com>
2020-06-15 19:09:02 -0400

    Add specular shading to cube

--- Sandbox/src/Sandbox/Shaders/Phong_PS.hlsl
+++ Sandbox/src/Sandbox/Shaders/Phong_PS.hlsl
@@ -1,16 +1,19 @@
 // Light properties
 
-static const float3 kLightPos = { 0.0f, 0.0f, -3.0f };
+static const float3 kLightPos = { 2.0f, 2.0f, 2.0f };
 static const float3 kLightColor = { 1.0f, 1.0f, 1.0f };
-static const float kLightIntensity = 0.5f;
+static const float kLightIntensity = 1.0f;
 
-static const float kAmbientLight = 0.2f;
+static const float kAmbientLight = 0.3f;
 
-static const float kAttnCoeff = 0.0075f;
+static const float kAttnCoeff0 = 1.0f;
+static const float kAttnCoeff2 = 0.0075f;
 
 // Object properties
 
 static const float3 kMaterialColor = { 0.6f, 0.2f, 0.2f };
+static const float kSpecularIntensity = 0.5f;
+static const float kSpecularPower = 10.0f;
 
 float4 main(float3 worldPos : POSITION, float3 normal : NORMAL) : SV_Target
 {
@@ -20,17 +23,24 @@ float4 main(float3 worldPos : POSITION, float3 normal : NORMAL) : SV_Target
     const float3 lCap = lVec / lNorm;
     
     // Calculate attenuation at this distance
-    const float attn = 1 / (kAttnCoeff * (lNorm * lNorm));
+    const float attn = 1 / (kAttnCoeff0 + kAttnCoeff2 * (lNorm * lNorm));
     
     // Calculate diffuse component
     const float diffuseLight = attn * kLightIntensity * max(0.0f, dot(lCap, normal));
     
+    // Calculate specular component
+    const float3 rCap = reflect(lCap, normal);
+    const float3 rCosine = max(0.0f, dot(rCap, normalize(worldPos)));
+    const float specularLight = attn * kSpecularIntensity * pow(rCosine, kSpecularPower);
+    
     // Reflected colour should only be whatever exists in both the light and the material
     const float3 diffuseColor = kLightColor * kMaterialColor;
     
-    // Observed object colour should be diffuse component * diffuse color
-    // plus ambient component * diffuse color
-    const float3 observedColor = saturate((diffuseLight + kAmbientLight) * diffuseColor);
+    // Observed object colour should be: 
+    // diffuse component * diffuse colour +
+    // ambient component * diffuse colour +
+    // specular component * light colour
+    const float3 observedColor = saturate((diffuseLight + kAmbientLight) * diffuseColor + specularLight * kLightColor);
     
     return float4(observedColor, 1.0f);
 }
