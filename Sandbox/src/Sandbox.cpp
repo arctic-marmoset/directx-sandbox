ac3dddb529a3b9a021012fc9299fb450821800a4
ArcticMarmoset <45583929+ArcticMarmoset@users.noreply.github.com>
2020-10-20 17:43:35 -0400

    Switch back to console subsystem

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,14 +1,11 @@
 #include "gspch.h"
 #include "Sandbox/Application.h"
 
-int main(int argc, char *argv[])
+int wmain(int argc, wchar_t *argv[])
 {
-    return wWinMain(GetModuleHandle(nullptr), nullptr, GetCommandLine(), SW_SHOWDEFAULT);
-}
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-{
+#ifdef NDEBUG
+    FreeConsole();
+#endif
     Application app;
     app.Run();
-    return 0;
 }
